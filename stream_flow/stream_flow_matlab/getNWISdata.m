870419b13bcf86c393ef93ce0fe64a34888abb56
Jim Thomson <jthomson@apl.washington.edu>
2024-04-08 18:42:29 -0700

    rating curves included
    also both sites

--- stream_flow/stream_flow_matlab/getNWISdata.m
+++ stream_flow/stream_flow_matlab/getNWISdata.m
@@ -2,27 +2,28 @@
 %
 % J. Thomson, Apr 2024
 
-gageno='12200500';
+gageno='12200500'; % Skagit
+%gageno='12061500'; % Skykomish
 
-OUTFILENAME = websave('streamdata.csv',['https://waterdata.usgs.gov/nwis/measurements?site_no=' gageno '&agency_cd=USGS&format=rdb_expanded'])
+OUTFILENAME = websave('streamdata.csv',['https://waterdata.usgs.gov/nwis/measurements?site_no=' gageno '&agency_cd=USGS&format=rdb_expanded']);
 
 
 %% read data, dealing with timestamps that may or may not have hh:mm:ss included
 
-headerlines = 16;
+headerlines = 17;
 linenum = 0;
 counter = 0;
-tline = 0
+tline = 0;
 
 fid = fopen(OUTFILENAME);
 
 while tline~=-1
     tline = fgetl(fid);
-    linenum = linenum + 1
+    linenum = linenum + 1;
     if length(tline)>6 && linenum > headerlines
         counter = counter+1;
-        thisdata = textscan(tline,'%s%n%n%s%s%s%s%s%s%s%s%s');%,'delimiter','\t');
-        datestring = char(thisdata{4})
+        thisdata = textscan(tline,'%s%s%s%s%s%s%s%s%s%s%s%s$s$s');%,'delimiter','\t');
+        datestring = char(thisdata{4});
         if length(char(thisdata{5}))~=8% all(char(thisdata{5}) == 'Yes') | all(char(thisdata{5}) == 'No')  % no hh:mm:ss, so columns are shifted
             timestring = '00:00:00';
             if ~isempty(str2num(char(thisdata{8})))
@@ -36,7 +37,7 @@ while tline~=-1
                 discharge(counter) = NaN;
             end
         else
-            timestring = char(thisdata{5})
+            timestring = char(thisdata{5});
             if ~isempty(str2num(char(thisdata{10})))
                 gageheight(counter) = str2num(char(thisdata{10}));
             else
@@ -49,7 +50,7 @@ while tline~=-1
             end
         end
         timestamp(counter) = datenum(str2num(datestring(1:4)), str2num(datestring(6:7)), str2num(datestring(9:10)), ...
-                str2num(timestring(1:2)), str2num(timestring(4:5)),str2num(timestring(7:8)) )
+                str2num(timestring(1:2)), str2num(timestring(4:5)),str2num(timestring(7:8)) );
     end
 end
 
@@ -62,13 +63,43 @@ gageheight (gageheight > 50 ) = NaN;
 %% Save output
 save(['MatlabData_gage' gageno '.mat'],'gageheight','discharge','timestamp')
 
-%% plot
+%% time series plot
 
 figure(1)
 subplot(2,1,1)
 plot(timestamp, gageheight), datetick
-ylabel('Gage Height')
+ylabel('Gage Height (units?)')
+title(gageno)
 
 subplot(2,1,2)
 plot(timestamp, discharge), datetick
-ylabel('Discharge')
+ylabel('Discharge (units?)')
+
+print('-dpng',['Timeseries_' gageno '.png'])
+
+%% scatter plot
+
+[year month day hour minute second] = datevec(timestamp);
+
+figure(2), clf
+scatter(gageheight, discharge, 10, year,'filled')
+axis([0 inf 0 inf])
+title(gageno)
+ylabel('Discharge (units?)')
+xlabel('Gage Height (units?)')
+cbar = colorbar; cb.Label.String = 'year';
+
+%% rating curve, latking the log of each side to enable a linear fit
+
+gooddata = find( gageheight>0 & discharge>0);
+offset = 0;
+
+P = polyfit( log( gageheight(gooddata) - offset ), log(discharge(gooddata)), 1 );  % first order polynomial fit... y = mx + b
+
+G = linspace(0,max(gageheight),10);
+Q = exp(P(2)) .* (G - offset ).^P(1);
+
+hold on % continue with previous figure
+ratingcurve = plot(G,Q,'k','linewidth',2)
+
+print('-dpng',['RatingCurve_' gageno '.png'])
